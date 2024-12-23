6cc2e32052fbec87dd4047b74e2c50827671814b
Mizan Ali Khan <mizanpathan3@gmail.com>
2024-12-24 03:32:37 +0530

    Added build commands

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,11 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import "dotenv/config";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
 import authRouter from "./routes/auth.route";
 import formRouter from "./routes/form.route";
+import path from "path";
 
 const app = express();
 const PORT = process.env.PORT || 8000;
@@ -19,8 +20,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+app.use(express.static(path.join(__dirname, "../../client/dist")));
+
 app.use("/api/auth", authRouter);
 app.use("/api/form", formRouter);
+
+app.get("*", (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, "../../client/dist/index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on Port ${PORT}`);
